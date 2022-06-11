df7f13127142ba786b7eaa1498b86675e60bf3ab
Carlos Meneses <cmeneses@espol.edu.ec>
2022-06-10 19:03:00 -0500

    Finishing

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -1,5 +1,5 @@
-//
-//
+// Copyright (C) 2020
+// All rights reserved
 
 package labcodingstandards;
 
@@ -7,18 +7,22 @@ import java.util.Scanner;
 
 
 /**
- * @author
- *
+ * @author Joel Alvarado
+ * 
  */
 public class Calculator {	
-	
+	/**
+	* Main function for the calculator
+	*@param args
+	*/
 	public static void main(String[] args) {
+
 		Scanner reader = new Scanner(System.in);
 		
         System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
         
         char operator = reader.nextLine().charAt(0);
-        double First;
+        double first;
         double second;
         String input;
         
@@ -27,7 +31,7 @@ public class Calculator {
         	input = reader.nextLine();
         	
             try {
-            	First=Integer.parseInt(input);
+            	first=Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
@@ -47,13 +51,20 @@ public class Calculator {
         }
 
         Calculator cal=new Calculator();
-        String result=cal.Operation(First, second,operator);
+        String result=cal.operation(First, second, operator);
 
         System.out.printf(result);
 		reader.close();
 	}
-
-	private String Operation(double first,double second,char operator) {
+	
+	/**
+	* Method to do the operation dependind on the string sent on operator
+	*@param first the first number
+	*@param second the second number
+	*@param operator the type of operator
+	*@return the result or error if there is an error
+	*/
+	private String operation(double first, double second, char operator) {
 		double result = 0;
 		switch(operator)
         {
